9c640635cc831472a278221208d7f0b1a9df0a5c
JinwooJung <iod1124@naver.com>
2022-03-21 01:02:41 +0900

    user db crud  기능 수정

--- models/index.js
+++ models/index.js
@@ -4,32 +4,64 @@ const {Config} = require('node-json-db/dist/lib/JsonDBConfig');
 const User =class{
     db;
     constructor(){
-        this.db= new JsonDB(new Config("db/database", true, false, "/"))
+        this.db= new JsonDB(new Config("db/database", true, false, "/"));
+        this.db.push("/user[]",{},true);
+        // this.db.push("/user[]",{
+        //     id:"01011111111",
+        //     name:"정진우",
+        //     birth:"930616"
+        // },true)
     }
-    find(phoneNum){
-        console.log(phoneNum)
-        const data = this.db.getData("/user");
-        const test = this.db.getIndex("/user",phoneNum);
-        console.log(JSON.stringify(test));
-        let result = {};
-        for(var i =0; i<data.length; i++){
-            const temp = data[i];
-            if(temp[phoneNum]){
-                result= temp[phoneNum];
-            }
+    select(phoneNum){
+        // const data = this.db.getData("/user");
+        // const test = this.db.getIndex("/user","phoneNum",phoneNum);
+        // const test1 = this.db.getData("/user/"+phoneNum);
+        
+        // return 1 / -1
+        const idx = this.db.getIndex("/user",phoneNum);
+        var result = {}
+        if(idx !== -1){
+            result['resultCode'] = 1
+            result['data'] = this.db.getData("/user["+idx+"]");
+        }else{
+            result["resultCode"] = -1 // 존재하지 않는 사용자
+            result['data'] = {};
         }
+        
+
+        // console.log(test);
+        // console.log(JSON.stringify(test));
+        // console.log(JSON.stringify(test1));
         
         return result;
     }
-    create(phoneNum, name, birth, email){
-        this.db.push("/user[]/"+phoneNum+"/", {
-            id:phoneNum,
-            name:name,
-            birth:birth,
-            email:email
-        },false);
+    insert(phoneNum, name, birth, email){
+        // const result = this.db.push("/user[]/"+phoneNum+"/", {
+        //     id:phoneNum,
+        //     name:name,
+        //     birth:birth,
+        //     email:email
+        // },false);
+        const idx = this.db.getIndex("/user",phoneNum);
+        var result = {};
+        if(idx === -1){
+            result['resultCode'] = 1
+            result['data'] = this.db.push("/user[]", {
+                id:phoneNum,
+                name:name,
+                birth:birth,
+                email:email
+            },true);
+        }else{
+            result["resultCode"] = -1 // 이미 있는 사용자
+            result['data'] = this.db.getData("/user["+idx+"]");
+        }
+      
+        console.log("create result:"+ result);
         this.db.save();
         this.db.reload();
+        // 위에 두개 필요한건지 check
+        return result;
     }
 
 }
--- routes/user.js
+++ routes/user.js
@@ -8,15 +8,33 @@ router
     .get('/', async(req, res, next)=>{
         const phoneNum = req.query.phoneNum;
         console.log(phoneNum)
-        const data = await user.find(phoneNum);
-        res.header("Content-Type: application/json")
-        res.send(data);
+        const result = await user.select(phoneNum);
+        res.header("Content-Type: application/json");
+        if(result['resultCode']===-1){
+            let error = {};
+            error['msg']="존재하지 않는 사용자";
+            return res.send(error);
+        }else{
+            let success = {};
+            success['msg']=result['data'];
+            return res.send(success);
+        }
     });
     router.post('/', async(req, res, next)=>{
         console.log("[User] post ")
         const body = req.body;
-        await user.create(body.phoneNum, body.name, body.birth, body.email);
-        
+        const result = await user.insert(body.phoneNum, body.name, body.birth, body.email);
+        console.log(result);
+        res.header("Content-Type: application/json");
+        if(result['resultCode'] === -1){
+            let error = {};
+            error["msg"]="존재하는 사용자"
+            return res.send(error);
+        }else{
+            let success = {};
+            success["msg"]="insert Completed!"
+            return res.send(success);
+        }
     })
 
 module.exports = router;
